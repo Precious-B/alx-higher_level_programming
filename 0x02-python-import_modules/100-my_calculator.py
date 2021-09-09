a500a71518f507793ea1f96c0754c8087b968a99
Precious-B <solomonprecious04@gmail.com>
2021-09-09 15:15:01 +0000

    Advanced

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    from sys import argv
+    n = len(argv)
+    if n != 4:
+        print("Usage: {} <a> <operator> <b>".format(argv[0]))
+        exit(1)
+    a = int(argv[1])
+    b = int(argv[3])
+    ops = ["+", "-", "*", "/"]
+    functions = [add, sub, mul, div]
+    for i, j in enumerate(ops):
+        if argv[2] == j:
+            print("{} {} {} = {}".format(a, j, b, functions[i](a, b)))
+            break
+        else:
+            print("Unknown operator. Available operators: +, -, * and /")
+            exit(1)
