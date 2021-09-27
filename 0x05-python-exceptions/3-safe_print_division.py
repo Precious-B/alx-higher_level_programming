8a2a13e3496c695aace9c853878128eb10459d0e
Precious-B <solomonprecious04@gmail.com>
2021-09-27 20:18:45 +0000

    integers division with debug

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    cmpt = 0
+    for index in range(x):
+        try:
+            print('{:d}'.format(my_list[index]), end='')
+            cmpt += 1
+        except IndexError:
+            break
+        except Exception:
+            pass
+    print('')
+    return cmpt
