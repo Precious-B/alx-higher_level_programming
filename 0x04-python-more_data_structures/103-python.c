77fe0bbecf418cb10268930c0198d78be3fa82c2
Precious-B <solomonprecious04@gmail.com>
2021-09-27 18:57:53 +0000

    Python exceptions

---
+++ 0x04-python-more_data_structures/103-python.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * print_python_list - a function that print some basic info about Python lists
+ *
+ * @p: a python list
+ */
+void print_python_list(PyObject *p)
+{
+}
+
+/**
+ * print_python_bytes - a function that print some basic info about Python bytes objects
+ *
+ * @p: Python bytes
+ */
+void print_python_bytes(PyObject *p)
+{
+}
